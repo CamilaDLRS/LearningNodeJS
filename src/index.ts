9726771f9d3e0233c81601c970d0d334847c0594
Camila Delarosa <milla.delarosa@hotmail.com>
2022-06-20 23:12:49 -0300

    creating fist function and runing the project

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,19 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server');
+function testandoFuction(req:any, res:any)
+{
+    console.log(req.query.banana);
+    console.log(req.query.laranja);
+    console.log(req.body.frutas);
+    res.send('renan feio');
+}
+
+app.get('/camila', (req, res) => {
+  res.send('ping');
 });
+
+app.post('/renan', testandoFuction);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at https://localhost:${port}`);
